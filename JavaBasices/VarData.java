34e523fd71ee5bd2a8c2ae09e668d964bd823d5c
karthikshetty63 <200613karthik@gmail.com>
2026-01-27 23:52:12 +0530

    day1:variables and Datatypes

---
+++ JavaBasices/VarData.java
@@ -0,0 +1,26 @@
+package JavaBasices;
+
+public class VarData {
+    public static void main(String[] args)
+
+    {
+        String name = "karthik shetty";
+        int age = 21;
+        float marks = 7.8f;
+        double height = 5.377;
+        boolean result = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Height: " + height);
+        System.out.println("Result: " + result);
+    }
+}
+// mistakes :pacakge name should be correct as folder name;
+// calss name shuld not have space or - ;
+// main method should be written correctly;
+// String should start with capital S;
+// all name age marks height result cannot print in one sytem.out println for
+// all should use seperate system.out.println as like abbove
+// class name should start with capital letter
